d757fc081dc820cbfd1a9b5b357d96927190ef41
Fae Hamilton-Raine <faehamiltonraine@gmail.com>
2025-02-06 10:06:20 -0700

    Some changes

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,36 @@ struct Account {
     holder: String,
 }
 
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Bank {
     accounts: Vec<Account>,
 }
 
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_account(account: &Account) {
+    println!("{:#?}", account);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let account = Account::new(1, String::from("me"));
+
+    print_account(&account);
+
+    println!("{:#?}", account);
+
 }
